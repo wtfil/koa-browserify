a1f97f6769319932dc193d4a8dcbb8ab1870f1e3
wtfil <filmuzhik@gmail.com>
2014-05-19 14:13:56 +0300

    added doc

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 koa-browserify
 ==============
+
+## install
+    
+    npm i koa-browserify
+
+## example
+
+```js
+var app = require('koa')(),
+    cjs = require('koa-browserify');
+
+app.use(cjs({
+    root: './public', // root folder for js files
+    maps: true // show sorcemap
+}));
+
+app.use(cjs('./public')) // equivalent to {root: './public'}
+
+app.listen(3000);
+```
+
+## production
+
+You can use this module in production by setting option ```production``` or ```env``` variable ```NODE_ENV``` to ```production```
+
+```js
+app.use(cjs({
+    root: './public',
+    maps: true,
+    production: true // forse set production mode
+}));
+```
+    
+or
+
+    NODE_ENV=production node app.js
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var JS_RE = /\.js$/,
     browserify = require('browserify'),
     thunkify = require('thunkify'),
+    uglify = require('uglify-js'),
     resolve = require('path').resolve;
 
 module.exports = function (options) {
@@ -10,25 +11,50 @@ module.exports = function (options) {
         options = {root: options};
     }
 
-    var root = resolve(options.root || '.');
+    var root = resolve(options.root || '.'),
+        isProduction = 'production' in options ? options.production : (process.env.NODE_ENV === 'production'),
+        bundleOpts = {
+            debug: (!isProduction && options.maps) || false
+        },
+        hash = {};
     
     return function* (next) {
+        var url = this.originalUrl,
+            minify, code;
 
-        if (!JS_RE.test(this.originalUrl)) {
+        if (!JS_RE.test(url)) {
             return yield next;
         }
 
-        var fileName = root + this.originalUrl,
+        if (isProduction && hash[url]) {
+            return this.body = hash[url];
+        }
+
+        var fileName = root + url,
             b = browserify();
 
         b.add(fileName);
 
         try {
-            this.body = yield thunkify(b.bundle.bind(b))();
+            code = yield thunkify(b.bundle.bind(b, bundleOpts))();
         } catch (e) {
             this.status= 500;
             this.body = e.stack;
         }
+
+        if (isProduction) {
+            try {
+                minify = uglify.minify(code, {fromString: true});
+                console.log(minify);
+                code = minify.code;
+            } catch(e) {
+                console.log(e.stack);
+            }
+
+            hash[url] = code;
+        }
+
+        this.body = code;
 
     };
 
